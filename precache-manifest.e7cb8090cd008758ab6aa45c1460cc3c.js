e1fe18711c2fdb68729306e697191fd2e5dbbb2f
Anastasiia Pavlenko <anastasia2ustimets@gmail.com>
2020-01-29 14:14:50 +0200

    Updates

---
+++ precache-manifest.e7cb8090cd008758ab6aa45c1460cc3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f813b3d68fd1c41873b3bc688138f28a",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "644bacab45795b259062",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.53ee0377.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.53ee0377.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8b0ca30c9a3e681e53a4",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.588745d3.chunk.js"
+  },
+  {
+    "revision": "5eea2426daa3643f03a9",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.6819ab10.js"
+  }
+]);
